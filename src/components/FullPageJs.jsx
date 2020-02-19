93773a7a1dc9467598e61fa78ddc996fddc031de
Sofiane <s-attilah@hotmail.com>
2020-02-19 23:02:59 +0100

    test fullPageJs

---
+++ src/components/FullPageJs.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactFullpage from '@fullpage/react-fullpage';
+import '../css/test.css';
+
+const FullPageJs = () => (
+
+  <ReactFullpage 
+  // ReactFullpage props
+  navigation
+  sectionsColor={["#282c34", "#ff5f45", "#0798ec"]}
+  render = {({state, fullpageApi}) => {
+    return (
+      <>
+      <ReactFullpage.Wrapper>
+      <div className="section">
+  
+      </div>
+      <div className="section">
+  
+      </div>
+      <div className="section">
+  
+      </div>
+      </ReactFullpage.Wrapper>
+      </>
+    );
+  }}
+  />
+)
+
+export default FullPageJs;
+
+
+
+
+
