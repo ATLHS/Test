c8efe3072ced1e902dcacffabaf2a62bcf6bf420
Sofiane <s-attilah@hotmail.com>
2020-02-28 16:06:48 +0100

    Video.jsx : window.innerWidth not innerHeight L7

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -4,7 +4,7 @@ import tesla_video_desktop from '../videos/tesla_video_desktop.mp4';
 import tesla_video_mobile from '../videos/tesla_video_mobile.mp4';
 
 const Video = () => {
-      const video_src = window.innerHeight > 700 ? tesla_video_desktop : tesla_video_mobile;
+      const video_src = window.innerWidth > 700 ? tesla_video_desktop : tesla_video_mobile;
       return (
             <div className="container" style={{height: window.innerHeight}}>
                   <video className="video" src={video_src} muted loop autoPlay playsInline></video>
